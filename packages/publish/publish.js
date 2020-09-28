37bc7606c5b7c90ee45c10d6e34fd68ba054940f
youping <youping@tsign.cn>
2020-09-28 17:27:42 +0800

    fix(publish): 添加try...catch

--- packages/publish/publish.js
+++ packages/publish/publish.js
@@ -3,18 +3,23 @@ const inquirer = require('inquirer')
 const shell = require('shelljs')
 
 async function publish(publishPackages, packages) {
-  for (let package of publishPackages) {
-    let curPackage = packages.find(item => {
-      return item.name === package.value
-    })
-
-    let code = shell.exec('npm publish ', {
-      cwd: curPackage.path || curPackage.packagePath
-    }).code
-    if (code !== 0) throw '发布失败..'
-    else console.log(`${curPackage.name} 发布成功.. `.green)
+  try {
+    for (let package of publishPackages) {
+      let curPackage = packages.find(item => {
+        return item.name === package.value
+      })
+  
+      let code = shell.exec('npm publish ', {
+        cwd: curPackage.path || curPackage.packagePath
+      }).code
+      if (code !== 0) throw '发布失败..'
+      else console.log(`${curPackage.name} 发布成功.. `.green)
+    }
+  } catch (error) {
+    
+  } finally {
+    console.log(`发布任务结束.. `.green)
   }
-  console.log(`所有package发布成功.. `.green)
 }
 
 module.exports = publish
